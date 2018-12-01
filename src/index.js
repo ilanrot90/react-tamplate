07a79690a0c6b9d0d423464594b27f84a24e8671
ilan90 <ilanrot90@gmail.com>
2018-12-01 22:08:34 +0200

    add rouer dom

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from "react-redux";
 import { configureStore } from "./store";
 import * as serviceWorker from './serviceWorker';
 
 const store = configureStore();
 
+// routes
+const pageA = () => <div>Page 1</div>
+const pageB = () => <div>Page 2</div>
+
+
 const App = () => {
     return (
-        <Provider store={store}>
-            <div>hello</div>    
+        <Provider store={store}>     
+            <Router>
+                <div>
+                    <Route path="/a" exact component={pageA}/>
+                    <Route path="/b" exact component={pageB}/>
+                </div>
+            </Router>
         </Provider>
     )
 }
