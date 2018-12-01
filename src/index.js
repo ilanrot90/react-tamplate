31aab7c0657001b307aca381381406e16fabf850
ilan90 <ilanrot90@gmail.com>
2018-12-01 21:49:46 +0200

    create tamplate with redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from "react-redux";
+import { configureStore } from "./store";
 import * as serviceWorker from './serviceWorker';
+
+const store = configureStore();
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <div>hello</div>    
+        </Provider>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/store/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+const init = (state, action) => ({});
+
+const rootReducer = combineReducers({
+    init
+})
+
+export default rootReducer
